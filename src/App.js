cc1aad2674c31e9b9f98af6ed206aeb6a2b7e14d
DennisDR <dennis.deroover@student.kdg.be>
2019-10-03 13:49:11 +0200

    adds footer init and react-final-from

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Nav from "./.core/navbar/Nav";
 import Home from "./home/Home";
+import Footer from "./footer/Footer";
 
 function App() {
   return (
     <div>
       <Nav />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer_wrapper">
+        <div>
+          <p>Form</p>
+        </div>
+        <div>
+          <p>Contactinfo</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;

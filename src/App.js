a19415a92cbf8efb2841773741fa67743cc5fd36
Yannick Slegers <yannickslegers2@msn.com>
2019-10-17 19:06:15 +0200

    update navbar and extra pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-import Nav from "./.core/navbar/Nav";
+import Navbar from "./.core/navbar/navbar";
 import Home from "./home/Home";
 import Footer from "./footer/Footer";
 
 function App() {
   return (
     <div>
-      <Nav />
+      <Navbar />
       <Home />
       <Footer />
     </div>

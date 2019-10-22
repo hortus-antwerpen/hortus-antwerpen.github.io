b06eff60fe420825edc187232f79351a8bef2819
DennisDR <dennis.deroover@student.kdg.be>
2019-10-22 21:16:24 +0200

    adds spinner on load

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-        , document.getElementById("root")
-    );
+const { useEffect } = React;
+
+const loader = document.querySelector(".bg-loader");
+
+// if you want to show the loader when React loads data again
+const showLoader = () => loader.classList.remove("loader--hide");
+
+const hideLoader = () => loader.classList.add("loader--hide");
+
+const Root = ({ hideLoader }) => {
+  useEffect(() => hideLoader(), []);
+
+  return <App />;
+};
+
+ReactDOM.render(<Root hideLoader={hideLoader} showLoader={showLoader} />, document.getElementById("root"));
